01e31f05ded31b60f4eb2a2dc05c56fa0e8ae865
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-04 10:43:18 -0700

    Added Preliminary methods

--- models/base_model.py
+++ models/base_model.py
@@ -8,23 +8,33 @@ from datetime import datetime
 
 # I think it inherits Cmd?
 class BaseModel():
-    """ BaseModel Superclass """
+    """
+    BaseModel Superclass
+    """
     def __init__(self):
-        """ Initialize values """
+        """
+        Initialize values
+        """
         self.id = uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """ returns string representation """
+        """
+        returns string representation
+        """
         return f"[{self.__class__}] ({self.id}) <{self.__dict__}>"
 
     def save(self):
-        """ Updates updated_at with current time """
+        """
+        Updates updated_at with current time
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ returns dictionary with instance attributes """
+        """
+        returns dictionary with instance attributes
+        """
         selfDict = self.__dict__
         selfDict.append({"__class__": self.__class__})
         selfDict['created_at'] = self['created_at'].isoformat()
