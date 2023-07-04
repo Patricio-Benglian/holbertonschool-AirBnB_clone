fa1c5fb3fdd01bb9fb818290ca2ccc822758b094
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-04 10:41:05 -0700

    Added Preliminary methods

--- models/base_model.py
+++ models/base_model.py
@@ -14,11 +14,11 @@ class BaseModel():
         self.id = uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def __str__(self):
         """ returns string representation """
         return f"[{self.__class__}] ({self.id}) <{self.__dict__}>"
-    
+
     def save(self):
         """ Updates updated_at with current time """
         self.updated_at = datetime.now()
