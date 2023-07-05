3adbede50bd4ef2f96becc9549f2a0ec56439da3
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-05 10:17:02 -0700

    Create file_storage to serialize and deserialize

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+file_storage module
+"""
+import json
+
+
+class FileStorage():
+    """ serializes and deserializes json """
+    __file_path = "file.json"
+    __objects = {} # dict. empty at first
+
+    def all(self):
+        """ returns __objects dict """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ sets obj with key in __objects """
+        key = obj.__class__.__name__ + obj.id
+        FileStorage.__objects.append({key: obj})
+
+    def save(self):
+        """ serializes __objects to JSON """
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
+            f = json.loads(str(FileStorage.__objects))
+
+    def reload(self):
+        """ deserializes JSON to __objects """
+        try:
+            with open(FileStorage.__file_path, mode="r", encoding="utf-8") as f:
+                f = json.dumps(FileStorage.__objects)
+        except FileNotFoundError:
+            pass
