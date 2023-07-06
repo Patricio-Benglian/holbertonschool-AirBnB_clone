de411e894c53ab0c749592ad0b4bd3c45b0a058f
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-06 09:07:14 -0700

    Fixed import

--- tests/test_models/test_number_one.py
+++ tests/test_models/test_number_one.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import unittest
 from datetime import datetime
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
 
