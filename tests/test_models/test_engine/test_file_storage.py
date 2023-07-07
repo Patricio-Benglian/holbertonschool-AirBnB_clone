edeae28ab899c8271ffbff3cdaa91e2b523d2b3e
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-07 07:20:49 -0700

    Push test file

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import unittest
+from datetime import datetime
+from models.engine.file_storage import FileStorage
+
+class test_FileStorage(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_to_dict(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+    
