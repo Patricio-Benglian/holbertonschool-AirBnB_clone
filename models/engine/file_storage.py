a6d0ab8f2122c360b542b9fd5bc76c6a1928a15b
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-08 14:43:15 -0700

    added Class Imports

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,6 +41,11 @@ class FileStorage():
         """
         from models.base_model import BaseModel
         from models.user import User
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.city import City
+        from models.review import Review
+        from models.state import State
         try:
             with open(FileStorage.__file_path, mode="r") as f:
                 objects = json.load(f)
