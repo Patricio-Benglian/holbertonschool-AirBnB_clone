c6b46283d926710d2f576b3723748b2addb8872a
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-08 08:40:35 -0700

    Push Tests

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+test_amenity module
+"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class test_Amenity(unittest.TestCase):
+    pass
---
+++ tests/test_models/test_place.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+test_place module
+"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class test_Place(unittest.TestCase):
+    pass
