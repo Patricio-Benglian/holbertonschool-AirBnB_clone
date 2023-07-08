c36e2ed905c8c93e6185b0231078511c05ba8dfb
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-08 08:32:16 -0700

    Push Tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-test_user module
+test_state module
 """
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
 
-class test_User(unittest.TestCase):
+class test_State(unittest.TestCase):
     pass
