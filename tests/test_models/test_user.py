27ab4ffad65a1f74b4f2eb4df275748d2e2841e1
Patricio-Benglian <6305@holbertonstudents.com>
2023-07-08 08:30:13 -0700

    Test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+test_user module
+"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class test_BaseModel(unittest.TestCase):
+    pass
